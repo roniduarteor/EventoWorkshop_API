c12de45983d0860ecb4abf85147810c30fdaf00e
Ronaldo Duarte <ronaldoduarteoliveirarodrigues@gmail.com>
2024-08-12 08:04:39 -0300

    :sparkles: feat: Rota get para palestrantes concluída!

--- src/controllers/palestranteController.js
+++ src/controllers/palestranteController.js
@@ -2,5 +2,17 @@ import conn from "../config/conn.js"
 import { v4 as uuidv4 } from 'uuid'
 
 export const getPalestrante = (request, response) =>{
-    response.status(200).json({message: "FOI"})
+    const sql = /*sql*/ `
+    select * from palestrantes
+    `
+
+    conn.query(sql, (err, data)=>{
+        if(err){
+            response.status(500).json({message: "Erro ao verificar palestrantes existentes"})
+            return console.log(err)
+        }
+
+        const palestrantes = data
+        response.status(200).json(palestrantes)
+    })
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import 'dotenv/config'
-import path from 'node:path'
 
 import conn from './config/conn.js'
 
