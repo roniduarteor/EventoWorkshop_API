2efa8ebbdac60b5333e64291252cd360ab056184
Ronaldo Duarte <ronaldoduarteoliveirarodrigues@gmail.com>
2024-08-12 07:43:15 -0300

    :sparkles: feat: configuração do server.js e .env

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import 'dotenv/config'
+import path from 'node:path'
+
+const PORT = process.env.PORT
+
+const app = express()
+app.use(express.json())
+
+
+// Usar as rotas que vão ser criadas aqui
+
+
+//404
+app.use((request, response) => {
+    response.status(404).json({ message: 'Recurso não encontrado' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor on PORT ${PORT}`)
+})
